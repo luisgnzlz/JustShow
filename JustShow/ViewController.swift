bcef6acba031af9ffe9ac3904a02ba3068e943ab
luisgnzlz <luisgnzlz33@gmail.com>
2024-11-07 11:03:57 -0800

    Initial Commit

---
+++ JustShow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustShow
+//
+//  Created by Luis Gonzalez on 11/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
---
+++ JustShowTests/JustShowTests.swift
@@ -0,0 +1,17 @@
+//
+//  JustShowTests.swift
+//  JustShowTests
+//
+//  Created by Luis Gonzalez on 11/7/24.
+//
+
+import Testing
+@testable import JustShow
+
+struct JustShowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
